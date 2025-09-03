f9ca946f5697fe43baf14f3862a022af48b51fc6
S0k1l <martynuky16@gmail.com>
2025-09-03 20:22:11 +0300

    Created layout component

---
+++ src/app/components/layout/layout.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { NavBar } from '../nav-bar/nav-bar';
+import { RouterOutlet } from '@angular/router';
+@Component({
+  selector: 'app-layout',
+  imports: [NavBar, RouterOutlet],
+  templateUrl: './layout.html',
+  styleUrl: './layout.css',
+})
+export class Layout {}
